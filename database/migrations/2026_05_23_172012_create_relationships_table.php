974eae1de57ec5dd51d7c213ba6259dd81a7a144
Antoine Torbey <anthonytorbey781@gmail.com>
2026-05-23 20:23:11 +0300

    fixed typo

--- database/migrations/2026_05_23_172012_create_relationships_table.php
+++ database/migrations/2026_05_23_172012_create_relationships_table.php
@@ -21,6 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('relatinships');
+        Schema::dropIfExists('relationships');
     }
 };
