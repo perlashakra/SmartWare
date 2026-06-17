c4d3a4b5c0922b517df132752a337391602a90a7
Antoine Torbey <anthonytorbey781@gmail.com>
2026-06-17 12:04:48 +0300

    swapped a single line

--- app/Models/UserPreference.php
+++ app/Models/UserPreference.php
@@ -7,7 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class UserPreference extends Model
 {
     protected $fillable = ['user_id', 'role', 'business_type'];
-    protected $casts = [
-        'product_type' => \App\Enums\ProductType::class,
-    ];
 }
