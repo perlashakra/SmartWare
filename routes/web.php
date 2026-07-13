6e508b8aa35bb2eb2a87af3d30f9f53c8b74fce7
Antoine Torbey <anthonytorbey781@gmail.com>
2026-07-13 17:43:20 +0300

    Initial admin requests

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::middleware(['auth', 'is_admin'])->prefix('admin/dashboard')->group(function () {
+    Route::post('/admins', [AdminController::class, 'createAdmin']);
+    Route::get('/requests/pending', [AdminController::class, 'pendingRequests']);
+    Route::get('/requests/{id}', [AdminController::class, 'showRequest']);
+    Route::post('/requests/{id}/review', [AdminController::class, 'reviewRequest']);
 });
