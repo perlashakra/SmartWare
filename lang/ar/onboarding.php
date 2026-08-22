dade16f32cc501f6ebdcf79dea4e39a53be14c08
Antoine Torbey <anthonytorbey781@gmail.com>
2026-08-22 06:00:56 +0300

    Some translations

--- lang/ar/onboarding.php
+++ lang/ar/onboarding.php
@@ -3,4 +3,13 @@ return [
     'role_required' => 'يرجى تحديد ما إذا كنت عميلاً أم مدير مستودع.',
     'business_type_required_if_client' => 'يجب على العملاء اختيار نوع النشاط التجاري.',
     'categories_required' => 'يجب عليك اختيار تفضيل واحد على الأقل لفئات المنتجات.',
+
+    'business_name_updated_successfully' => 'تم تحديث اسم المنشأة بنجاح.',
+    'legal_compliance_error_message' => 'بموجب القانون السوري، يُحظر تماماً تخزين سلع تجارية أو غير طبية بشكل مشترك في المستودعات المخصصة لتخزين المستلزمات الطبية أو الأدوية.',
+    'security_error_message' => 'واحدة أو أكثر من الفئات المحددة غير صالحة لفئة النشاط التجاري التي اخترتها.',
+    'preferences_saved_successfully' => 'تم حفظ التفضيلات بنجاح.',
+
+    'onboarding_documents_uploaded' => 'تم رفع مستندات التهيئة وتقديمها بنجاح.',
+    'identity_document_uploaded' => 'تم تحميل وثيقة الهوية بنجاح.',
+    'facility_document_uploaded' => 'تم تحميل مستند المنشأة بنجاح.',
 ];
